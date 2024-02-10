ee4e4dc7982011ffe742f11e77591fd02eca347e
juned-shaikh <juned.shaikh.7415@gmail.com>
2024-02-10 19:54:46 +0530

    website done

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDwFYzJfQVg25H8PcqxXxivhmJeLy3lJNY",
+        authDomain: "junedportfolio.firebaseapp.com",
+        databaseURL: "https://junedportfolio-default-rtdb.firebaseio.com",
+        projectId: "junedportfolio",
+        storageBucket: "junedportfolio.appspot.com",
+        messagingSenderId: "828204032766",
+        appId: "1:828204032766:web:9f36671ee0ba342a8a3baf",
+        measurementId: "G-ZD6MVTXPGV"
+    }
+  };
